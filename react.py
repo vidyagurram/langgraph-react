3fea29946d344fe993bb253bee22a00ac8c6cf91
Vidya Gurram <gurramvidya@gmail.com>
2026-08-11 14:28:25 -0500

    llm and Tools creation

---
+++ react.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+@tool
+def triple(num:float) -> float:
+    """
+    param num: an input number to get tripled
+    result: triple of an input number
+    """
+
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0).bind_tools(tools)
+
+
+
+
+
+load_dotenv(override=True)
